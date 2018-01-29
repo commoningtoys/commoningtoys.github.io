6f816aca30ab98458daf0642386e3a5708774016
Yann Patrick Martins <yannpatrickmartins@ba14am32015.adm.ds.fhnw.ch>
2018-01-29 17:21:32 +0100

    add enalrging divs on click add repositioning of the divs according to the menu height

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 let myClasses = ["team", "project", "sBlog", "moodBoard"];
+// let myTags = ['yano', 'micha', 'shintaro', 'selena', 'viktor', 'kayla']
 /**
  * Object to store visibility and name of classes
  * @param {String} _name - name of the class
  * @param {boolean} _visible - boolean to set the visibility of the class
  */
-function ClassObject(_name, _visible){
+function ClassObject(_name, _visible) {
     this.name = _name;
     this.visible = _visible;
 }
 
 let ClassObjects = [];
-for(let i = 0; i < myClasses.length; i++){
+for (let i = 0; i < myClasses.length; i++) {
     ClassObjects.push(new ClassObject(myClasses[i], true));
 }
 
@@ -18,41 +19,98 @@ for(let i = 0; i < myClasses.length; i++){
  * function that reveals only certain elements of the website
  * @param {String} className - the class that needs to be set to 'display: none;' 
  */
-function reveal(className){
-    //here we set the visibility of the classes
-    //we set them to true if the className matches with a ClassOject
-    for(let i = 0; i < ClassObjects.length; i++){
-        if(ClassObjects[i].name.includes(className)){
+function reveal(className) {
+    // showAll();
+    console.log('imWOrking');
+    for (let i = 0; i < ClassObjects.length; i++) {
+        if (ClassObjects[i].name.includes(className)) {
             ClassObjects[i].visible = true;
             let thisClass = document.getElementsByClassName(ClassObjects[i].name);
-            for(let j = 0; j < thisClass.length; j++){
-                    thisClass[j].style.display = 'block';
-                }
-        }else{
+            for (let j = 0; j < thisClass.length; j++) {
+                thisClass[j].style.display = 'block';
+            }
+        } else {
             ClassObjects[i].visible = false;
             let thisClass = document.getElementsByClassName(ClassObjects[i].name);
-            for(let j = 0; j < thisClass.length; j++){
-                    thisClass[j].style.display = 'none';
-                }
+            for (let j = 0; j < thisClass.length; j++) {
+                thisClass[j].style.display = 'none';
+            }
+        }
+    }
+}
+/**
+ * reveals elements based on tag name
+ * @param {String} tagName - name of the tag to be searched
+ */
+function revealTag(tagName) {
+    showAll();
+    for (let i = 0; i < myTags.length; i++) {
+        if (myTags[i].includes(tagName)) {
+            // myTags[i].visible = true;
+            let thisClass = document.getElementsByTagName(myTags[i]);
+            for (let j = 0; j < thisClass.length; j++) {
+                thisClass[j].style.display = 'block';
+            }
+        } else {
+            // myTags[i].visible = false;
+            let thisClass = document.getElementsByTagName(myTags[i]);
+            for (let j = 0; j < thisClass.length; j++) {
+                thisClass[j].style.display = 'none';
+            }
+        }
+    }
+}
+/**
+ * function that shows all the posts of the wesite
+ */
+function showAll() {
+    for (let i = 0; i < myClasses.length; i++) {
+        let thisClass = document.getElementsByClassName(myClasses[i]);
+        for (let j = 0; j < thisClass.length; j++) {
+            thisClass[j].style.display = 'block';
         }
     }
 }
 /**
  * this function initializes the width and heights of all the divs
  */
-function init(){
-    for(let i = 0; i < myClasses.length; i++){
+function init() {
+    for (let i = 0; i < myClasses.length; i++) {
         let thisClass = document.getElementsByClassName(myClasses[i]);
-        for(let j = 0; j < thisClass.length; j++){
-            let randW = Math.random() * 60;
-            let randH = Math.random() * 500;
-            let randM = Math.random() * 5;
+        for (let j = 0; j < thisClass.length; j++) {
+            let randW = Random(20, 60);
             thisClass[j].style.width = Math.floor(randW) + '%';
-            thisClass[j].style.height = Math.floor(randH) +'px';
-            thisClass[j].style.margin = Math.floor(randM) +'%';
+            let randH = thisClass[j].style.width;//Random();
+            // console.log(randH);
+            let randM = Random(0.1, 2);
+            thisClass[j].style.height = Math.floor(randH) + 'px';
+            // thisClass[j].style.margin = randM +'%';
 
         }
     }
+}
+/**
+ * stes the position of the divs
+ */
+function setPositionDivs() {	
+    let scale = window.devicePixelRatio;
+    console.log(scale);
+    /**
+    * testing jQuery functions
+    */
+   $("a").click(function (className) {
+       console.log(this.innerHTML);
+   });
+   let menuH = $("#myMenu").height();
+   console.log(menuH);
+   document.getElementById("myContainer").style.top = menuH * scale * 2/3 + 'px';
+   console.log('divs in position!')
+   // $("myContainer").css(top, menuH);
+   // console.log(menuH);
+    // console.log('le madonne!')
+    // let menuHeight = document.getElementById('myMenu').style.height;
+    // console.log(menuHeight);
+    // document.getElementById('myContainer').style.top = menuHeight;
 }
 /**
  * this function returns a random number between two numbers
@@ -60,9 +118,9 @@ function init(){
  * @param {float} b 
  * @returns {float} random number between the input numbers
  */
-function Random(a, b){
+function Random(a, b) {
     //if a is smaller than b we should swap them
-    if(b < a){
+    if (b < a) {
         let Swap = b;
         b = a;
         a = Swap;
@@ -70,3 +128,36 @@ function Random(a, b){
     let c = Math.abs(a - b);
     return a + (Math.random() * c);
 }
+/**
+ * function to show hide the menu
+ */
+let menuIsShow = true;
+function closeOpenMenu() {
+    menuIsShow = !menuIsShow;//every click we change the status of the boolean
+    let myTags = document.getElementsByTagName('a');
+    for (let i = 0; i < myTags.length; i++) {
+        menuIsShow == false ? myTags[i].style.display = 'none' : myTags[i].style.display = 'initial';
+    }
+    let menuClass = document.getElementsByClassName('menu');
+    for (let i = 0; i < menuClass.length; i++) {
+        menuIsShow == false ? menuClass[i].style.width = 'auto' : menuClass[i].style.width = '100%';
+    }
+    let button = document.getElementById('openClose');
+    menuIsShow == true ? button.innerHTML = 'Close Menu' : button.innerHTML = 'Show Menu';
+}
+
+// $(".project").click(function(e){
+//     let h = window.innerHeight;
+//     console.log(e);
+//     // e.target.style.width = '98%';
+//     // e.target.style.height = h + 'px';
+// });
+function enlarge(e){
+    console.log(e);
+}
+// /**
+//  * testing jQuery functions
+//  */
+// $( "a" ).click(function () {
+//     console.log("jQuery");
+// });
--- sketch.js
+++ sketch.js
@@ -1,8 +1,35 @@
-
+let pos, target, vel, moving = false, c1, c2;
 function setup() {
-
+    noCanvas();
+    pos = createVector(0, 0);
+    target = createVector(0, 0);
+    vel = createVector(0, 0);
+    c1 = color(255, 85, 0);
+    c2 = color(0, 204, 255);
 }
 
 function draw() {
+    if (moving){
+        seek(target);
+        window.scrollTo(pos.x, pos.y);
+        if(targetReached())moving = false;        
+    }
+}
 
+function goTo(x, y){
+    moving = true;
+    target = createVector(x, y);
+}
+
+function targetReached(){
+    return p5.Vector.dist(pos, target) < 5;
+}
+
+function seek(vector){
+    let dir = p5.Vector.sub(target, pos);
+    dir.normalize();
+    dir.mult(5);
+    vel.add(dir);
+    vel.limit(10);
+    pos.add(vel);
 }
